91420a24a8339a1589bbd5f7bcb8cfe3be6e95ee
zh <izhulien@yahoo.com>
2016-01-10 19:21:30 +0200

    Refactor the view models by removing the unused ImageLocation property

--- CV.Common/ViewModels/LanguageFrameworkDetailedViewModel.cs
+++ CV.Common/ViewModels/LanguageFrameworkDetailedViewModel.cs
@@ -6,8 +6,6 @@
 
         public string Name { get; set; }
 
-        public string ImageLocation { get; set; }
-
         public int TutorialsWatchedScore { get; set; }
 
         public int ProblemsSolvedScore { get; set; }
